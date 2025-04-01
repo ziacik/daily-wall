447a1f6fef27c1504fa892d866a2c86569f882f4
František Žiačik <ziacik@gmail.com>
2025-04-01 22:05:35 +0200

    chore: reformat all, remove e2e

--- apps/dwall/jest.config.ts
+++ apps/dwall/jest.config.ts
@@ -3,19 +3,19 @@ import { readFileSync } from 'fs';
 
 // Reading the SWC compilation config for the spec files
 const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
+	readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
 );
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: 'dwall',
-  preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: 'test-output/jest/coverage',
+	displayName: 'dwall',
+	preset: '../../jest.preset.js',
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.[tj]s$': ['@swc/jest', swcJestConfig],
+	},
+	moduleFileExtensions: ['ts', 'js', 'html'],
+	coverageDirectory: 'test-output/jest/coverage',
 };
--- apps/dwall/src/main.ts
+++ apps/dwall/src/main.ts
@@ -1,11 +1,47 @@
-import OpenAI from "openai";
+import axios from 'axios';
+import fs from 'fs';
+import OpenAI from 'openai';
+import path from 'path';
+import { setWallpaper } from 'wallpaper';
+// import terminalImage from "terminal-image";
 
 const openai = new OpenAI();
 
 async function main() {
-  const image = await openai.images.generate({ model: "dall-e-3", size: "1792x1024", prompt: "Generate a colorful wallpaper featuring a photorealistic character designed with a style inspired by anime. The character is a young girl with large, expressive eyes and long hair styled in soft waves. She is dressed in a cute dress and her pose is random. The girl's layer is small and features her entire body, placed on a clean, spacious background. As a unique touch, incorporate something in the scene that subtly represents the current date." });
+	const image = await openai.images.generate({
+		model: 'dall-e-3',
+		size: '1792x1024',
+		prompt:
+			"Generate a colorful wallpaper featuring a photorealistic character designed with a style inspired by anime. The character is a young girl with large, expressive eyes and long hair styled in soft waves. She is dressed in a cute dress and her pose is random. The girl's layer is small and features her entire body, placed on a clean, spacious background. As a unique touch, incorporate something in the scene that subtly represents the current date.",
+	});
 
-  console.log(image.data);
+	console.log(image);
+
+	if (image.data[0].url) {
+		// download the image to file
+		const imageUrl = image.data[0].url;
+		const imagePath = path.resolve('./wallpaper.jpg');
+		const writer = fs.createWriteStream(imagePath);
+
+		const response = await axios({
+			url: imageUrl,
+			method: 'GET',
+			responseType: 'stream',
+		});
+
+		response.data.pipe(writer);
+
+		await new Promise((resolve, reject) => {
+			writer.on('finish', resolve);
+			writer.on('error', reject);
+		});
+
+		// console.log(await terminalImage.file(imagePath));
+
+		await setWallpaper(imagePath);
+	}
+
+	console.log(image.data);
 }
 
 main();
