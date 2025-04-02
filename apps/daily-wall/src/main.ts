7d22937701a6e5927b4b0eccdfcb6bc60fca927b
František Žiačik <frantisek.ziacik@resco.net>
2025-04-02 13:38:51 +0200

    feat: prompt adjusted a bit

--- apps/daily-wall/src/main.ts
+++ apps/daily-wall/src/main.ts
@@ -14,7 +14,7 @@ async function main() {
 		model: 'dall-e-3',
 		size: '1792x1024',
 		prompt:
-			"Generate a colorful wallpaper featuring a photorealistic character designed with a style inspired by anime. The character is a young girl with large, expressive eyes and long hair styled in soft waves. She is dressed in a cute dress and her pose is random. The girl's layer is small and features her entire body, placed on a clean, spacious background. As a unique touch, incorporate something in the scene that subtly represents the current date.",
+			"Generate a colorful wallpaper featuring a photorealistic character designed with a style inspired by anime. The character is a young girl with large, expressive eyes and long hair styled in soft waves. She is dressed in a cute dress and her pose is random. The girl's layer is small and features her entire body, placed on a clean, spacious background.",
 	});
 
 	console.log(image);
