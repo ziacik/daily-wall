49ba2e4337cb9b210b1186ab0d3937c69a01cad6
František Žiačik <frantisek.ziacik@resco.net>
2025-04-14 09:33:46 +0200

    feat: retry added

--- apps/daily-wall/src/main.ts
+++ apps/daily-wall/src/main.ts
@@ -5,33 +5,55 @@ import fs from 'fs';
 import OpenAI from 'openai';
 import path from 'path';
 import { setWallpaper } from 'wallpaper';
-// import terminalImage from "terminal-image";
 
 const openai = new OpenAI();
 
-async function main() {
-	const image = await openai.images.generate({
-		model: 'dall-e-3',
-		size: '1792x1024',
-		prompt:
-			"Generate a colorful wallpaper featuring a photorealistic character designed with a style inspired by anime. The character is a young girl with large, expressive eyes and long hair styled in soft waves. She is dressed in a cute dress and her pose is random. The girl's layer is small and features her entire body, placed on a clean, spacious background, maybe on the side of it. The background should have a nice color.",
-	});
+async function retryAsync<T>(
+	fn: () => Promise<T>,
+	retries = 3,
+	delay = 60000
+): Promise<T> {
+	let lastError: unknown;
+	for (let attempt = 1; attempt <= retries; attempt++) {
+		try {
+			return await fn();
+		} catch (error) {
+			lastError = error;
+			console.warn(`Attempt ${attempt} failed.`);
+			await wait(delay);
+		}
+	}
+	throw new Error(`Failed after ${retries} attempts. Reason: ${lastError}`);
+}
 
-	console.log(image);
+async function wait(delay: number) {
+	return new Promise((resolve) => setTimeout(resolve, delay));
+}
+
+async function main() {
+	const image = await retryAsync<OpenAI.Images.ImagesResponse>(() =>
+		openai.images.generate({
+			model: 'dall-e-3',
+			size: '1792x1024',
+			prompt:
+				"Generate a colorful wallpaper featuring a photorealistic character designed with a style inspired by anime. The character is a young girl with large, expressive eyes and long hair styled in soft waves. She is dressed in a cute dress and her pose is random. The girl's layer is small and features her entire body, placed on a clean, spacious background, maybe on the side of it. The background should have a nice color.",
+		})
+	);
 
 	if (image.data[0].url) {
-		// download the image to file
 		const imageUrl = image.data[0].url;
 
 		const currentDate = new Date().toISOString().split('T')[0];
 		const imagePath = path.resolve(`./daily-wall-${currentDate}.jpg`);
 		const writer = fs.createWriteStream(imagePath);
 
-		const response = await axios({
-			url: imageUrl,
-			method: 'GET',
-			responseType: 'stream',
-		});
+		const response = await retryAsync(() =>
+			axios({
+				url: imageUrl,
+				method: 'GET',
+				responseType: 'stream',
+			})
+		);
 
 		response.data.pipe(writer);
 
@@ -40,12 +62,8 @@ async function main() {
 			writer.on('error', reject);
 		});
 
-		// console.log(await terminalImage.file(imagePath));
-
 		await setWallpaper(imagePath);
 	}
-
-	console.log(image.data);
 }
 
 main();
